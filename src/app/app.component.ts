642d4d36d4e2bc8b5e6cf0e2dc4ae797d6e48a95
Nathália Elen <nathaliaelenestudos@gmail.com>
2026-07-20 13:54:46 -0300

    chore: criação e estilização dos componentes primary e secondary button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './components/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
