2afd080fd77b9d8c00acd304386156ece66ef519
Nathália Elen <n.fialho.de.araujo@avanade.com>
2026-07-22 15:37:29 -0300

    chore: usando o NgModel  no formulário.

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../components/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
   imports: [
     SecondaryButtonComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    FormsModule
   ],
   templateUrl: './certificado-form.component.html',
-  styleUrl: './certificado-form.component.css'
+  styleUrls: ['./certificado-form.component.css']
 })
 export class CertificadoFormComponent {
-
+  nomeCompleto: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Vue'];
 }
