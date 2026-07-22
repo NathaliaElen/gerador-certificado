c4c796ca0697058920748e1de23beb189d444e76
Nathália Elen <n.fialho.de.araujo@avanade.com>
2026-07-22 17:56:49 -0300

    chore: validando campos do formulário

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../../components/secondary-button/secondary-button.component';
 import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
   imports: [
     SecondaryButtonComponent,
     PrimaryButtonComponent,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './certificado-form.component.html',
   styleUrls: ['./certificado-form.component.css']
@@ -17,4 +19,10 @@ export class CertificadoFormComponent {
   nomeCompleto: string = '';
   atividade: string = '';
   atividades: string[] = ['Angular', 'React', 'Vue'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid &&
+      control.touched &&
+      control.errors?.['required'];
+  }
 }
